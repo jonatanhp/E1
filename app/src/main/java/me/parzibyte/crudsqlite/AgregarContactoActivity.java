7824d38ab50b2fdf5cf49978edc3e624181198b6
jonatanhp <jonatan.hp@upeu.edu.pe>
2022-05-09 20:03:02 -0500

    second

--- app/src/main/java/me/parzibyte/crudsqlite/AgregarContactoActivity.java
+++ app/src/main/java/me/parzibyte/crudsqlite/AgregarContactoActivity.java
@@ -7,62 +7,72 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import me.parzibyte.crudsqlite.controllers.MascotasController;
-import me.parzibyte.crudsqlite.modelos.Mascota;
+import me.parzibyte.crudsqlite.controllers.ContactosController;
+import me.parzibyte.crudsqlite.modelos.Contacto;
 
-public class AgregarMascotaActivity extends AppCompatActivity {
-    private Button btnAgregarMascota, btnCancelarNuevaMascota;
-    private EditText etNombre, etEdad;
-    private MascotasController mascotasController;
+public class AgregarContactoActivity extends AppCompatActivity {
+    private Button btnAgregarContacto, btnCancelarNuevaContacto;
+    private EditText etNombre, etApellido, etTelefono, etEmail;
+    private ContactosController contactosController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_agregar_mascota);
+        setContentView(R.layout.activity_agregar_contacto);
 
         // Instanciar vistas
         etNombre = findViewById(R.id.etNombre);
-        etEdad = findViewById(R.id.etEdad);
-        btnAgregarMascota = findViewById(R.id.btnAgregarMascota);
-        btnCancelarNuevaMascota = findViewById(R.id.btnCancelarNuevaMascota);
+        etApellido = findViewById(R.id.etApellido);
+        etTelefono = findViewById(R.id.etTelefono);
+        etEmail = findViewById(R.id.etEmail);
+        btnAgregarContacto = findViewById(R.id.btnAgregarContacto);
+        btnCancelarNuevaContacto = findViewById(R.id.btnCancelarNuevaContacto);
         // Crear el controlador
-        mascotasController = new MascotasController(AgregarMascotaActivity.this);
+        contactosController = new ContactosController(AgregarContactoActivity.this);
 
         // Agregar listener del botón de guardar
-        btnAgregarMascota.setOnClickListener(new View.OnClickListener() {
+        btnAgregarContacto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Resetear errores a ambos
                 etNombre.setError(null);
-                etEdad.setError(null);
+                etApellido.setError(null);
+                etTelefono.setError(null);
+                etEmail.setError(null);
                 String nombre = etNombre.getText().toString(),
-                        edadComoCadena = etEdad.getText().toString();
+                       String apellido = etApellido.getText().toString(),
+                       String telefono = etTelefono.getText().toString(),
+                       String email = etEmail.getText().toString();
                 if ("".equals(nombre)) {
-                    etNombre.setError("Escribe el nombre de la mascota");
+                    etNombre.setError("Escribe el nombre del contacto");
                     etNombre.requestFocus();
                     return;
                 }
-                if ("".equals(edadComoCadena)) {
-                    etEdad.setError("Escribe la edad de la mascota");
-                    etEdad.requestFocus();
+                if ("".equals(apellido)) {
+                    etApellido.setError("Escribe el apellido del contacto");
+                    etApellido.requestFocus();
                     return;
                 }
 
-                // Ver si es un entero
-                int edad;
-                try {
-                    edad = Integer.parseInt(etEdad.getText().toString());
-                } catch (NumberFormatException e) {
-                    etEdad.setError("Escribe un número");
-                    etEdad.requestFocus();
+                if ("".equals(telefono)) {
+                    etTelefono.setError("Escribe el teléfono del contacto");
+                    etTelefono.requestFocus();
                     return;
                 }
+
+                if ("".equals(email)) {
+                    etEmail.setError("Escribe el email del contacto");
+                    etEmail.requestFocus();
+                    return;
+                }
+
+                // Crear el contacto
                 // Ya pasó la validación
-                Mascota nuevaMascota = new Mascota(nombre, edad);
-                long id = mascotasController.nuevaMascota(nuevaMascota);
+                Contacto nuevaContacto = new Contacto(nombre, apellido, telefono, email);
+                long id = contactosController.nuevaContacto(nuevaContacto);
                 if (id == -1) {
                     // De alguna manera ocurrió un error
-                    Toast.makeText(AgregarMascotaActivity.this, "Error al guardar. Intenta de nuevo", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(AgregarContactoActivity.this, "Error al guardar. Intenta de nuevo", Toast.LENGTH_SHORT).show();
                 } else {
                     // Terminar
                     finish();
@@ -71,7 +81,7 @@ public class AgregarMascotaActivity extends AppCompatActivity {
         });
 
         // El de cancelar simplemente cierra la actividad
-        btnCancelarNuevaMascota.setOnClickListener(new View.OnClickListener() {
+        btnCancelarNuevaContacto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
